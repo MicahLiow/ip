5ebb6199c4d260491b5d9eb875125504bfdff66a
MicahLiow <micah.liow@gmail.com>
2026-08-31 12:47:00 +0800

    Allow chat to store and load list to/from file
    Orangutan bot now stores the list to data/orangutan.txt
    On new session, it also loads any pre-existing data from data/orangutan.txt
    
    .gitignore: set to ignore /data/ folder

--- src/main/java/Orangutan.java
+++ src/main/java/Orangutan.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+/***************** TODO!!!!! class header comments, put all classes in packages*/
 
 public class Orangutan {
-    public static void main(String[] args) throws OrangutanException{
-        //initialize important variables
+    public static void main(String[] args) {
         //ascii banner adapted from https://ascii.co.uk/art/orangutan
         String line = "___________________________________________________________\n";
-        ChatList listItems = new ChatList();
 
         String banner = line
                       + "  ___                             | |             \n"
@@ -17,6 +21,22 @@ public class Orangutan {
                       + "                       |___/                      \n\n"
                       + "Greetings, I am Orangutan.\nHow may I assist you on this fine day?\n" + line;
         System.out.println(banner);
+
+        //initialize list from save file if one exists. If not, initialize an empty list
+        String path = "./data/orangutan.txt"; //path to save file
+        Path filePath = Paths.get(path);
+
+        ChatList listItems = null;
+        if (Files.exists(filePath)) {
+            try {
+                listItems = ChatList.readFromFile(filePath);
+            } catch (IOException e) {
+                System.out.println("Alas! I have failed to access the information previously stored in data/orangutan.txt.\n\n" +
+                        "Please ensure I have access to said file.");
+            }
+        } else {
+            listItems = new ChatList();
+        }
 
         //Get user input
         Scanner input = new Scanner(System.in);
@@ -161,9 +181,17 @@ public class Orangutan {
                         break;
 
                     case "bye":
-                        isExit = true;
+                        //writes listItems to data/orangutan.txt, then exits the loop
+                        try {
+                            isExit = true; //will only exit if we can successfully write to file
+                            listItems.writeToFile(filePath); //will automatically create directories and files as needed
+                            System.out.println("Fare thee well, and may we meet again.");
+                        } catch (IOException e) {
+                            isExit = false;
+                            System.out.println("Alas! I was not able to write your list to data/orangutan.txt.\n\n" +
+                                    "Please ensure I have access to said files and folders before we bid farewell.");
+                        }
 
-                        System.out.println("Fare thee well, and may we meet again.");
                         break;
 
                     default:
