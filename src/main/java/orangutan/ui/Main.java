0790012822e7b3fc1faaa32207030e9d98f45dde
MicahLiow <micah.liow@gmail.com>
2026-09-07 18:54:53 +0800

    Set window minimum size
    Previously window could be resized to be arbitrarily small.
    
    Let's set the minimum size a window can be resized to.

--- src/main/java/orangutan/ui/Main.java
+++ src/main/java/orangutan/ui/Main.java
@@ -22,6 +22,8 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
 
             //set taskbar icon and title
             //icon taken from www.flaticon.com/free-icon/orangutan_7743169
