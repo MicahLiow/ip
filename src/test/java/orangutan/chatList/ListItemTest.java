cdc1835c9a091f5d046ffa9b53f94b2352cf7d85
MicahLiow <micah.liow@gmail.com>
2026-09-15 00:08:36 +0800

    Junit: include testing for whitespace in title

--- src/test/java/orangutan/chatList/ListItemTest.java
+++ src/test/java/orangutan/chatList/ListItemTest.java
@@ -7,106 +7,106 @@ import org.junit.jupiter.api.Test;
 public class ListItemTest {
     @Test
     public void todoTest() {
-        ListItem test = new ListItem(ItemType.TODO, "abc", false);
-        assertEquals("[T] [ ] abc", test.toString());
+        ListItem test = new ListItem(ItemType.TODO, "ab c", false);
+        assertEquals("[T] [ ] ab c", test.toString());
     }
 
     @Test
     public void deadlineTest() {
-        ListItem test = new ListItem(ItemType.DEADLINE, "abc", false, "20260831 1900");
-        assertEquals("[D] [ ] abc (by 2026-08-31 19:00)", test.toString());
+        ListItem test = new ListItem(ItemType.DEADLINE, "ab c", false, "20260831 1900");
+        assertEquals("[D] [ ] ab c (by 2026-08-31 19:00)", test.toString());
     }
 
     @Test
     public void eventTest() {
-        ListItem test = new ListItem(ItemType.EVENT, "abc", false, "20260831 1900", "20260831 2200");
-        assertEquals("[E] [ ] abc (from 2026-08-31 19:00 to 2026-08-31 22:00)", test.toString());
+        ListItem test = new ListItem(ItemType.EVENT, "ab c", false, "20260831 1900", "20260831 2200");
+        assertEquals("[E] [ ] ab c (from 2026-08-31 19:00 to 2026-08-31 22:00)", test.toString());
     }
 
     @Test
     public void unCompletedTest() {
-        ListItem test = new ListItem(ItemType.TODO, "abc", false);
-        assertEquals("[T] [ ] abc", test.toString());
+        ListItem test = new ListItem(ItemType.TODO, "ab c", false);
+        assertEquals("[T] [ ] ab c", test.toString());
     }
 
     @Test
     public void completedTest() {
-        ListItem test = new ListItem(ItemType.TODO, "abc", true);
-        assertEquals("[T] [X] abc", test.toString());
+        ListItem test = new ListItem(ItemType.TODO, "ab c", true);
+        assertEquals("[T] [X] ab c", test.toString());
     }
 
     @Test
     public void markTest() {
-        ListItem test = new ListItem(ItemType.TODO, "abc", false);
+        ListItem test = new ListItem(ItemType.TODO, "ab c", false);
         test.mark();
-        assertEquals("[T] [X] abc", test.toString());
+        assertEquals("[T] [X] ab c", test.toString());
     }
 
     @Test
     public void unmarkTest() {
-        ListItem test = new ListItem(ItemType.TODO, "abc", true);
+        ListItem test = new ListItem(ItemType.TODO, "ab c", true);
         test.unmark();
-        assertEquals("[T] [ ] abc", test.toString());
+        assertEquals("[T] [ ] ab c", test.toString());
     }
 
     @Test
     public void parseTodoTest() {
-        ListItem test = ListItem.parseLine("[T],false,abc");
-        assertEquals("[T] [ ] abc", test.toString());
+        ListItem test = ListItem.parseLine("[T],false,ab c");
+        assertEquals("[T] [ ] ab c", test.toString());
     }
 
     @Test
     public void parseDeadlineTest() {
-        ListItem test = ListItem.parseLine("[D],false,abc,20260831 1700");
-        assertEquals("[D] [ ] abc (by 2026-08-31 17:00)", test.toString());
+        ListItem test = ListItem.parseLine("[D],false,ab c,20260831 1700");
+        assertEquals("[D] [ ] ab c (by 2026-08-31 17:00)", test.toString());
     }
 
     @Test
     public void parseEventTest() {
-        ListItem test = ListItem.parseLine("[E],false,abc,20260831 1700,20260831 1900");
-        assertEquals("[E] [ ] abc (from 2026-08-31 17:00 to 2026-08-31 19:00)", test.toString());
+        ListItem test = ListItem.parseLine("[E],false,ab c,20260831 1700,20260831 1900");
+        assertEquals("[E] [ ] ab c (from 2026-08-31 17:00 to 2026-08-31 19:00)", test.toString());
     }
 
     @Test
     public void parseUncompletedTest() {
-        ListItem test = ListItem.parseLine("[T],false,abc");
-        assertEquals("[T] [ ] abc", test.toString());
+        ListItem test = ListItem.parseLine("[T],false,ab c");
+        assertEquals("[T] [ ] ab c", test.toString());
     }
 
     @Test
     public void parseCompletedTest() {
-        ListItem test = ListItem.parseLine("[T],true,abc");
-        assertEquals("[T] [X] abc", test.toString());
+        ListItem test = ListItem.parseLine("[T],true,ab c");
+        assertEquals("[T] [X] ab c", test.toString());
     }
 
     @Test
     public void todoToFileTest() {
-        String test = new ListItem(ItemType.TODO, "abc", false).toFile();
-        assertEquals("[T],false,abc", test);
+        String test = new ListItem(ItemType.TODO, "ab c", false).toFile();
+        assertEquals("[T],false,ab c", test);
     }
 
     @Test
     public void deadlineToFileTest() {
-        String test = new ListItem(ItemType.DEADLINE, "abc", false, "20260831 1900").toFile();
-        assertEquals("[D],false,abc,20260831 1900", test);
+        String test = new ListItem(ItemType.DEADLINE, "ab c", false, "20260831 1900").toFile();
+        assertEquals("[D],false,ab c,20260831 1900", test);
     }
 
     @Test
     public void eventToFileTest() {
-        String test = new ListItem(ItemType.EVENT, "abc", false, "20260831 1900", "20260831 2200")
+        String test = new ListItem(ItemType.EVENT, "ab c", false, "20260831 1900", "20260831 2200")
                 .toFile();
-        assertEquals("[E],false,abc,20260831 1900,20260831 2200", test);
+        assertEquals("[E],false,ab c,20260831 1900,20260831 2200", test);
     }
 
     @Test
     public void uncompletedToFileTest() {
-        String test = new ListItem(ItemType.TODO, "abc", false).toFile();
-        assertEquals("[T],false,abc", test);
+        String test = new ListItem(ItemType.TODO, "ab c", false).toFile();
+        assertEquals("[T],false,ab c", test);
     }
 
     @Test
     public void completedToFileTest() {
-        String test = new ListItem(ItemType.TODO, "abc", true).toFile();
-        assertEquals("[T],true,abc", test);
+        String test = new ListItem(ItemType.TODO, "ab c", true).toFile();
+        assertEquals("[T],true,ab c", test);
     }
 }
--- src/test/java/orangutan/chatlist/ChatListTest.java
+++ src/test/java/orangutan/chatlist/ChatListTest.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.Test;
 public class ChatListTest {
     @Test
     public void toStreamTest() {
-        ListItem todo = new ListItem(ItemType.TODO, "abc", false);
-        ListItem deadline = new ListItem(ItemType.DEADLINE, "abc", false, "20260831 1900");
-        ListItem event = new ListItem(ItemType.EVENT, "abc", true, "20260831 1900", "20260831 2200");
+        ListItem todo = new ListItem(ItemType.TODO, "ab c", false);
+        ListItem deadline = new ListItem(ItemType.DEADLINE, "ab c", false, "20260831 1900");
+        ListItem event = new ListItem(ItemType.EVENT, "ab c", true, "20260831 1900", "20260831 2200");
         ChatList test = new ChatList(new ArrayList<ListItem>(List.of(todo, deadline, event)));
         Stream<ListItem> testStream = test.toStream();
         assertEquals(Stream.of(todo, deadline, event).toList(), testStream.toList());
@@ -21,44 +21,44 @@ public class ChatListTest {
 
     @Test
     public void addItemTest() {
-        ListItem deadline = new ListItem(ItemType.DEADLINE, "abc", false, "20260831 1900");
-        ListItem event = new ListItem(ItemType.EVENT, "abc", true, "20260831 1900", "20260831 2200");
+        ListItem deadline = new ListItem(ItemType.DEADLINE, "ab c", false, "20260831 1900");
+        ListItem event = new ListItem(ItemType.EVENT, "ab c", true, "20260831 1900", "20260831 2200");
         ChatList test = new ChatList(new ArrayList<ListItem>(List.of(deadline)));
         test.addItem(event);
-        assertEquals("1. [D] [ ] abc (by 2026-08-31 19:00)\n2. [E] [X] abc (from 2026-08-31 19:00 to 2026-08-31 22:00)",
+        assertEquals("1. [D] [ ] ab c (by 2026-08-31 19:00)\n2. [E] [X] ab c (from 2026-08-31 19:00 to 2026-08-31 22:00)",
                 test.toString());
     }
 
     @Test
     public void deleteItemTest() {
-        ListItem deadline = new ListItem(ItemType.DEADLINE, "abc", false, "20260831 1900");
-        ListItem event = new ListItem(ItemType.EVENT, "abc", true, "20260831 1900", "20260831 2200");
+        ListItem deadline = new ListItem(ItemType.DEADLINE, "ab c", false, "20260831 1900");
+        ListItem event = new ListItem(ItemType.EVENT, "ab c", true, "20260831 1900", "20260831 2200");
         ChatList test = new ChatList(new ArrayList<ListItem>(List.of(deadline, event)));
         test.deleteItem(2);
-        assertEquals("1. [D] [ ] abc (by 2026-08-31 19:00)", test.toString());
+        assertEquals("1. [D] [ ] ab c (by 2026-08-31 19:00)", test.toString());
     }
 
     @Test
     public void markItemTest() {
-        ListItem event = new ListItem(ItemType.EVENT, "abc", false, "20260831 1900", "20260831 2200");
+        ListItem event = new ListItem(ItemType.EVENT, "ab c", false, "20260831 1900", "20260831 2200");
         ChatList test = new ChatList(new ArrayList<ListItem>(List.of(event)));
         test.markItem(1);
-        assertEquals("1. [E] [X] abc (from 2026-08-31 19:00 to 2026-08-31 22:00)", test.toString());
+        assertEquals("1. [E] [X] ab c (from 2026-08-31 19:00 to 2026-08-31 22:00)", test.toString());
     }
 
     @Test
     public void unmarkItemTest() {
-        ListItem event = new ListItem(ItemType.EVENT, "abc", true, "20260831 1900", "20260831 2200");
+        ListItem event = new ListItem(ItemType.EVENT, "ab c", true, "20260831 1900", "20260831 2200");
         ChatList test = new ChatList(new ArrayList<ListItem>(List.of(event)));
         test.unmarkItem(1);
-        assertEquals("1. [E] [ ] abc (from 2026-08-31 19:00 to 2026-08-31 22:00)", test.toString());
+        assertEquals("1. [E] [ ] ab c (from 2026-08-31 19:00 to 2026-08-31 22:00)", test.toString());
     }
 
     @Test
     public void getLengthTest() {
-        ListItem todo = new ListItem(ItemType.TODO, "abc", false);
-        ListItem deadline = new ListItem(ItemType.DEADLINE, "abc", false, "20260831 1900");
-        ListItem event = new ListItem(ItemType.EVENT, "abc", true, "20260831 1900", "20260831 2200");
+        ListItem todo = new ListItem(ItemType.TODO, "ab c", false);
+        ListItem deadline = new ListItem(ItemType.DEADLINE, "ab c", false, "20260831 1900");
+        ListItem event = new ListItem(ItemType.EVENT, "ab c", true, "20260831 1900", "20260831 2200");
         ChatList test = new ChatList(new ArrayList<ListItem>(List.of(todo, deadline, event)));
         assertEquals(3, test.getLength());
     }
